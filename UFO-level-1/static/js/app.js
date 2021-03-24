8201064ce12af2dd2724c19ebaeae7e1bf1556e0
Chelsea Ball <chelseaball@Chelseas-MacBook-Air.local>
2021-03-24 14:01:14 -0700

    Used d3 to update table data with UFO sighting data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//Get a reference to the table body
+var tbody = d3.select("tbody");
+
+//Use arrow functions and d3 to update each cell with UFO Sighting data
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
